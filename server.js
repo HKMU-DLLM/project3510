2f70f56326845c7a046a3964d1d60a4ea0bf6690
Kenny3311 <laikenny195@gmail,com>
2026-03-25 23:48:59 +0800

    add concert route and buy ticket page; remove login page

--- server.js
+++ server.js
@@ -5,15 +5,17 @@ const http = require("http");
 const app = express();
 const server = http.createServer(app);
 
+const concertRouter = require("./routes/concert");
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   console.log("New access to website");
-  res.status(200).render("login");
+  res.status(200).render("main");
 });
-
+app.use("/concert", concertRouter);
 const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);

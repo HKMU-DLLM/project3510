3121863aa5c85c9b40d7eb3b29fb868cf8222b01
Ka1sa1 <vincentbaobaoli@gmail.com>
2026-03-27 14:29:25 +0800

    Add support of language selection.

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const http = require("http");
+const i18n = require("i18n");
 const app = express();
+i18n.configure({
+  locales: ["en", "zh"],
+  directory: path.join(__dirname, "locales"),
+  defaultLocale: "en",
+  queryParameter: "lang"
+});
+
+app.use(i18n.init);
 const server = http.createServer(app);
+
 
 const concertRouter = require("./routes/concert");
 
@@ -13,7 +23,10 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   console.log("New access to website");
-  res.status(200).render("main");
+  res.status(200).render("main", {
+    title: res.__("title"),
+    welcome: res.__("welcome")
+  });
 });
 app.use("/concert", concertRouter);
 const PORT = 3000;

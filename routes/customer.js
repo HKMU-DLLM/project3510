7c65e275d2e997d03cd39f8fa0554d894885b4f3
WanXianNa <lujiaying0506@gmail.com>
2026-04-15 04:49:17 +0800

    feat: Login in/register

--- routes/customer.js
+++ routes/customer.js
@@ -1,5 +1,73 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../database/database.js");
+const isCustomer = (req, res, next) => {
+    if (req.session && req.session.isLoggedIn && req.session.user.role === 'customer') {
+        return next();
+    } else {
+        return res.redirect('/customer');
+    }
+};
+
+router.get("/", (req, res) => {
+    res.render("customer/customer_login");
+});
+
+router.get("/register", (req, res) => {
+    res.render("customer/customer_register");
+});
+
+// 处理注册（纯原生，无任何依赖）
+router.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+
+    try {
+        if (!name || !email || !password) {
+            return res.redirect('/customer/register');
+        }
+
+        db.prepare(`
+            INSERT INTO Customer (name, email, password, role)
+            VALUES (?, ?, ?, 'customer')
+        `).run(name, email, password);
+
+        res.redirect('/customer');
+
+    } catch (err) {
+        res.render('/customer/register');
+    }
+});
+
+// 处理登录（纯原生）
+router.post("/login", (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        if (username === "admin" && password === "admin123") {
+            req.session.isLoggedIn = true;
+            req.session.user = { role: "admin" };
+            return res.redirect('/admin/profile');
+        }
+
+        
+        const user = db.prepare(`
+            SELECT * FROM Customer 
+            WHERE name = ? AND password = ?
+        `).get(username, password);
+
+        if (!user) {
+            return res.redirect('/customer');
+        }
+
+        req.session.isLoggedIn = true;
+        req.session.user = user;
+        return res.redirect('/customer/buy_ticket');
+
+    } catch (err) {
+        res.redirect('/customer');
+    }
+});
+
 
 router.get("/buy_ticket", (req, res) => {
     res.render("customer/buy_ticket");

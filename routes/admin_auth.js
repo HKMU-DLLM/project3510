fe6835add9b7cf2ecedfa6464e7d95ce67a404df
Puro0629 <WASD6610122@gmail.com>
2026-04-18 13:57:23 +0800

    commit

--- routes/admin_auth.js
+++ routes/admin_auth.js
@@ -128,4 +128,13 @@ router.post("/edit/:concertId", isAdmin, (req, res) => {
     }
 });
 
+router.get("/logout", (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            return res.status(500).send("An error occurred while logging out.");
+        }
+        res.redirect('/customer');
+    });
+});
+
 module.exports = router;
